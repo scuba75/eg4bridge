2ee5c59e1fa3d753d56834c340d0e4fd42d85ee2
scuba <scuba75@gmail.com>
2026-04-10 20:32:07 -0500

    update

--- src/dataList.js
+++ src/dataList.js
@@ -1,6 +1,9 @@
-let dataList = {}
+let dataList = { "1": { "serial": process.env.INVERTER1_SN }, "2": { "serial": process.env.INVERTER2_SN } }
 const log = require('./logger')
 const cache = require('./sqlite')
+
+const SYNC_INTERVAL = (process.env.SYNC_INTERVAL_SECONDS || 20)
+
 function zonedTimestamp(timeZone = "America/New_York") {
   const parts = Object.fromEntries(
     new Intl.DateTimeFormat("en-US", {
@@ -22,7 +25,7 @@ const saveData = async()=>{
   try{
     let data = JSON.parse(JSON.stringify(dataList)), key = zonedTimestamp()
     if(!data?.main || !key?.date || !key?.time) return
-    
+
     let payload = {...key, ...data}
     await cache.set(key.date, payload, 'daily')
     await cache.set(`${key.date}_${key.time}`, payload, 'minute')
@@ -33,7 +36,7 @@ const saveData = async()=>{
 const sync = async()=>{
   try{
     await saveData()
-    setTimeout(sync, 5000)
+    setTimeout(sync, SYNC_INTERVAL * 1000)
   }catch(e){
     setTimeout(sync, 5000)
     log.error(e)
